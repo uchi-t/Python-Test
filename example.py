485a43d7c03e1796d41715adc209e62f9d132e83
aya <rare.tomo038@gmail.com>
2025-09-04 00:15:47 +0900

    ステップ150テスト

---
+++ example.py
@@ -0,0 +1,23 @@
+print("Hello World")
+
+def greet():
+    print("こんにちは")
+
+greet()
+
+def print_name(name):
+    print(f"私の名前は{name}です")
+
+print("一郎")
+print("次郎")
+
+def get_greet():
+    return "おはようございます"
+
+print(get_greet())
+
+def add(a,b):
+    return(a+b)
+
+print(add(4,7))
+
